667e4c4331ad053e28d1b0903761d38e8de99d9e
Gustavo Michels de Camargo <gustavo.camargo@strokmatic.com>
2023-08-30 11:31:59 -0300

    Adicionando as estruturas base

---
+++ rs-grafo/target/debug/deps/rs_grafo-33dfe934990a25c0.d
@@ -0,0 +1,5 @@
+/home/gus-skm/Vídeos/grafo-estrutura/rs-grafo/target/debug/deps/rs_grafo-33dfe934990a25c0.rmeta: src/main.rs
+
+/home/gus-skm/Vídeos/grafo-estrutura/rs-grafo/target/debug/deps/rs_grafo-33dfe934990a25c0.d: src/main.rs
+
+src/main.rs:
---
+++ rs-grafo/target/debug/deps/rs_grafo-b211d1aa67d86193.d
@@ -0,0 +1,5 @@
+/home/gus-skm/Vídeos/grafo-estrutura/rs-grafo/target/debug/deps/rs_grafo-b211d1aa67d86193.rmeta: src/main.rs
+
+/home/gus-skm/Vídeos/grafo-estrutura/rs-grafo/target/debug/deps/rs_grafo-b211d1aa67d86193.d: src/main.rs
+
+src/main.rs:
